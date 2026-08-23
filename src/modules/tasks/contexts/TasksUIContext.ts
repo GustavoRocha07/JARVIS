7869ac75d5ebbc61e386d08ebf4fe7fb9adb9c4c
Gustavo.Rocha <62334910+GustavoRocha07@users.noreply.github.com>
2026-08-23 01:46:09 -0300

    feat: control task modal mode

--- src/modules/tasks/contexts/TasksUIContext.ts
+++ src/modules/tasks/contexts/TasksUIContext.ts
@@ -1,15 +1,25 @@
 import { createContext } from "react";
 
-import type { Task, TasksParamsSearch } from "../types/tasks.type";
-import type { } from "../types/tasks.type";
+import type {
+    Task,
+    TaskModalMode,
+    TasksParamsSearch,
+} from "../types/tasks.type";
 
 export type TasksUIContextValue = {
     openModal: boolean;
+    modalMode: TaskModalMode;
     openConfirmDeletedModal: boolean;
     selectedTask: Task | null;
     filters: TasksParamsSearch;
 
-    handleOpenModal: (task?: Task) => void;
+    handleOpenModal: (
+        task?: Task,
+        mode?: TaskModalMode,
+    ) => void;
+    handleSetModalMode: (
+        mode: TaskModalMode,
+    ) => void;
     handleConfirmDeletedTask: (task: Task) => void;
     handleCloseModal: () => void;
     handleCloseConfirmDeletedModal: () => void;
