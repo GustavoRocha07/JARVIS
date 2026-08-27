72f89fa8779d284d74d0e7eed25e6eb9006deea6
Gustavo.Rocha <62334910+GustavoRocha07@users.noreply.github.com>
2026-08-27 16:19:55 -0300

    refactor: remove pagination context

--- src/context/PaginationContext/PaginationContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import type { PaginationContextValue, } from "./PaginationContext.type";
-
-export const PaginationContext = createContext<PaginationContextValue | undefined>(undefined)
-
